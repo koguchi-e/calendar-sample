4cb4e39ac20b2205aa75e92454a87c2778c1f445
Erika.K <koguchi.e915@gmail.com>
2025-03-16 12:44:56 +0000

    first commit

---
+++ app/javascript/packs/calendar.js
@@ -0,0 +1,28 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth'
+  });
+
+  calendar.render();
+});
+
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import './calendar'; // 追加
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
