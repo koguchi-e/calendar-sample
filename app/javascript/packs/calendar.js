d7099dfbd64ca2848d07e3df1aaeb320080cfebe
Erika.K <koguchi.e915@gmail.com>
2025-03-18 12:47:06 +0900

    [Update]カレンダー機能表示成功

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -3,26 +3,11 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
-
   var calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridMonth'
+    plugins: [ dayGridPlugin ],
+    initialView: 'dayGridMonth',
+    events: eventData // eventData変数を使用してイベントデータを渡す
   });
 
   calendar.render();
 });
-
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import './calendar'; // 追加
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
