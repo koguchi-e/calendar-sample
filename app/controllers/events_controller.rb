61d90c39bf13a6a13a33925e847ba367fce41a25
Erika.K <koguchi.e915@gmail.com>
2025-03-18 15:07:08 +0900

    [Update]日本語対応

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,9 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
+    respond_to do |format|
+      format.html
+      format.json { render 'calender' }
+    end
   end
 end
